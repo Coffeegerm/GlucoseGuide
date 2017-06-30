dce765b5975bc6a4dcb5b424e8a99f69d16b7143
David Yarzebinski <dyarzebinski97@gmail.com>
2017-06-29 23:19:35 -0400

    Set up news feed fragment

--- app/src/main/java/io/github/coffeegerm/materiallogbook/ui/NewsFragment.java
+++ app/src/main/java/io/github/coffeegerm/materiallogbook/ui/NewsFragment.java
@@ -3,10 +3,14 @@ package io.github.coffeegerm.materiallogbook.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import io.github.coffeegerm.materiallogbook.R;
 
 /*
@@ -17,10 +21,27 @@ import io.github.coffeegerm.materiallogbook.R;
 
 public class NewsFragment extends Fragment {
 
+    String diabetesCoUkRelatedArticleLinks = "http://www.diabetes.co.uk/News/rss/newsindex.xml";
+
+    @BindView(R.id.newsRecView)
+    RecyclerView newsRecyclerView;
+
+    @BindView(R.id.newsSwipeRefresh)
+    SwipeRefreshLayout newsSwipeRefresh;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View newsView = inflater.inflate(R.layout.fragment_news, container, false);
+
+        newsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        newsSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
 
         return newsView;
     }
