97b5db976376268be49250eb330c110b16fe282f
David Yarzebinski <dyarzebinski97@gmail.com>
2017-07-02 23:15:50 -0400

    DatePickerDialog, Will change

--- app/src/main/java/io/github/coffeegerm/materiallogbook/ui/NewEntryActivity.java
+++ app/src/main/java/io/github/coffeegerm/materiallogbook/ui/NewEntryActivity.java
@@ -1,12 +1,16 @@
 package io.github.coffeegerm.materiallogbook.ui;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,11 +47,38 @@ public class NewEntryActivity extends AppCompatActivity {
     @BindView(R.id.new_entry_insulin_units)
     EditText newEntryInsulin;
 
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_entry_activity);
         ButterKnife.bind(this);
+
+        newEntryDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(NewEntryActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog,
+                        mDateSetListener,
+                        year, month, day);
+
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Log.i(TAG, "onDateSet: Date set");
+                newEntryDate.setText(month + "/" + dayOfMonth + "/" + year);
+            }
+        };
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +99,7 @@ public class NewEntryActivity extends AppCompatActivity {
             }
         });
     }
+
 
     // TODO Create Realm database
 
