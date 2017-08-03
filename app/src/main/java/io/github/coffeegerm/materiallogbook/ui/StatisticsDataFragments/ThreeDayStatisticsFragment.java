b695b7d1a6a73f9a65910bf32ffa0d9b7ae4a061
David Yarzebinski <dyarzebinski97@gmail.com>
2017-08-03 18:26:18 -0400

    Layout updates and ThreeDaysStatistics Update

--- app/src/main/java/io/github/coffeegerm/materiallogbook/ui/StatisticsDataFragments/ThreeDayStatisticsFragment.java
+++ app/src/main/java/io/github/coffeegerm/materiallogbook/ui/StatisticsDataFragments/ThreeDayStatisticsFragment.java
@@ -3,14 +3,21 @@ package io.github.coffeegerm.materiallogbook.ui.StatisticsDataFragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.coffeegerm.materiallogbook.R;
+import io.github.coffeegerm.materiallogbook.model.EntryItem;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * Created by David Yarzebinski on 7/28/2017.
@@ -23,8 +30,15 @@ public class ThreeDayStatisticsFragment extends Fragment {
 
     private static final String TAG = "ThreeDaysStatistics";
 
+    @BindView(R.id.three_days_lowest)
+    TextView lowestGlucloseTextView;
+    @BindView(R.id.three_days_highest)
+    TextView highestGlucoseTextView;
+    @BindView(R.id.three_days_average)
+    TextView averageTextView;
     private String pageTitle;
     private int pageNumber;
+    private Realm realm;
 
     public static ThreeDayStatisticsFragment newInstance(int pageNumber, String pageTitle) {
         ThreeDayStatisticsFragment threeDayStatisticsFragment = new ThreeDayStatisticsFragment();
@@ -45,9 +59,25 @@ public class ThreeDayStatisticsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_three_days_stats, container, false);
+        View threeDaysStatisticsView = inflater.inflate(R.layout.fragment_three_days_stats, container, false);
+        ButterKnife.bind(this, threeDaysStatisticsView);
+        realm = Realm.getDefaultInstance();
+        Date threeDaysAgo = getThreeDaysAgo();
+        RealmResults<EntryItem> entriesFromThreeDaysAgo = realm.where(EntryItem.class).greaterThan("date", threeDaysAgo).findAllSorted("date");
+        Log.i(TAG, "onCreateView: " + entriesFromThreeDaysAgo);
+        return threeDaysStatisticsView;
+    }
 
-        return view;
+    public int getAverageGlucose() {
+        return 0;
+    }
+
+    public int getHighestGlucose() {
+        return 0;
+    }
+
+    public int getLowestGlucose() {
+        return 0;
     }
 
     public Date getThreeDaysAgo() {
