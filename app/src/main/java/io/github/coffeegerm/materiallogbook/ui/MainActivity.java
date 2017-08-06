71bded0fee3babcd9e1018dab974b4be9a3a74ae
David Yarzebinski <dyarzebinski97@gmail.com>
2017-08-06 11:56:55 -0400

    Exit Message

--- app/src/main/java/io/github/coffeegerm/materiallogbook/ui/MainActivity.java
+++ app/src/main/java/io/github/coffeegerm/materiallogbook/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.coffeegerm.materiallogbook.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
@@ -9,6 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -67,7 +69,17 @@ public class MainActivity extends AppCompatActivity
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            new AlertDialog.Builder(this)
+                    .setTitle("Close Material Logbook?")
+                    .setMessage("Do you really want to close Material Logbook?")
+                    .setPositiveButton("Get me out of here!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
         }
     }
 
